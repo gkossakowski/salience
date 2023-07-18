433f8be8f606b2dd07734deb74b9f882c8a72b94
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2023-07-18 18:31:31 +0200

    Move highlight rendering to server-side.
    The JS impl was very slow on larger docs. Refactor by GPT4 (ty!).

--- salience/__init__.py
+++ salience/__init__.py
@@ -1,18 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, make_response
+from .salience import extract, terminal_distr
 import numpy as np
-from .salience import extract
-import json
 
 app = Flask(__name__)
+
+@app.template_global()
+def scale(score):
+    return max(0, min(1, score ** 3 - 0.95))
 
 with open('./transcript.txt', 'r') as file:
     source_text = file.read().strip()
 sentence_ranges, adjacency = extract(source_text)
+scores = terminal_distr(adjacency)
 
-@app.route("/salience")
+@app.route("/salience", methods=['GET'])
 def salience_view():
-    return json.dumps({
-        'source': source_text,
-        'intervals': sentence_ranges,
-        'adjacency': np.nan_to_num(adjacency.numpy()).tolist(),
-    })
+    try:
+        sentences = [(score, source_text[start:end]) for score, (start, end) in zip(scores, sentence_ranges)]
+        return render_template('salience.html', sentences=sentences)
+    except Exception as e:
+        return make_response({'error': str(e)}, 500)
